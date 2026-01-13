376096dc641a9756f5bad643573a26ede2099a3c
ajqbt2025 <ajqbt2025@gmail.com>
2026-01-13 14:39:47 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const User = require("../models/User");
+const User = require("../Models/User");
 
 //auth
 exports.auth = async (req, res, next) => {

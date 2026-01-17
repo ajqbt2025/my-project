3522cdd168ef848359d102173b98a41a9f513d84
ajqbt2025 <ajqbt2025@gmail.com>
2026-01-17 12:31:34 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -7,7 +7,7 @@ function Signup() {
       title="Join the millions learning to code with StudyNotion for free"
       description1="Build skills for today, tomorrow, and beyond."
       description2="Education to future-proof your career."
-      image={signupImg}
+     
       formType="signup"
     />
   )

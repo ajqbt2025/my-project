c254e8f43bb2200e1a99803e0bb07e4f7949ad36
ajqbt2025 <ajqbt2025@gmail.com>
2026-01-27 21:47:46 +0530

    Update OpenRoute.jsx

--- src/components/core/Auth/OpenRoute.jsx
+++ src/components/core/Auth/OpenRoute.jsx
@@ -1,23 +1,19 @@
-// This will prevent authenticated users from accessing this route
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom"
 
 function OpenRoute({ children }) {
-  const { token } = useSelector((state) => state.auth);
-  const { user } = useSelector((state) => state.profile); // ya state.auth.user agar wahi hai
+  const { token } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.profile)
 
-  // Not logged in → allow access
   if (!token) {
-    return children;
+    return children
   }
 
-  // Logged in via Google but password not set → allow access to set password page
-  if (user && !user.password) {
-    return children;
+  if (user && user.isPasswordSet === false) {
+    return children
   }
 
-  // Otherwise → already has password → redirect to dashboard
-  return <Navigate to="/dashboard/my-profile" />;
+  return <Navigate to="/dashboard/my-profile" />
 }
 
-export default OpenRoute;
+export default OpenRoute
